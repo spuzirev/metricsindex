02f811c731d3ded53c3fd1fc54b02b1b5168b36a
Sergei Puzyrev <spuzirev@gmail.com>
2018-01-17 10:50:12 +0100

    Added stub of MetricsIndex API

--- metricsindex.go
+++ metricsindex.go
@@ -1,15 +1,11 @@
 package metricsindex
 
 import (
-	"errors"
-
 	"github.com/spuzirev/metricsindex/trees/metric_id_to_metric"
-	"github.com/spuzirev/metricsindex/trees/metric_ids"
 	"github.com/spuzirev/metricsindex/trees/tag_name_id_to_metric_ids"
 	"github.com/spuzirev/metricsindex/trees/tag_name_id_to_tag_values"
 	"github.com/spuzirev/metricsindex/trees/tag_name_value_id_to_metric_ids"
 	"github.com/spuzirev/metricsindex/trees/tag_names"
-	"github.com/spuzirev/metricsindex/trees/tag_values"
 	"github.com/spuzirev/metricsindex/types"
 )
 
@@ -41,6 +37,53 @@ func NewMetricsIndex() *MetricsIndex {
 	}
 }
 
+func (mi *MetricsIndex) insertMetric(metric types.Metric) error {
+	return nil
+}
+
+func (mi *MetricsIndex) InsertMetric(metricStr string) error {
+	return nil
+}
+
+func (mi *MetricsIndex) InsertMetricsBatch(metricStr []string) error {
+	return nil
+}
+
+type MetricIDIterator struct{}
+
+func (midi *MetricIDIterator) Next() (types.MetricID, error) {
+	return types.MetricID(0), nil
+}
+
+func (mi *MetricsIndex) GetMetricIDsIteratorByTag(tagNameStr, tagValueStr string) (*MetricIDIterator, error) {
+	return nil, nil
+}
+
+func (mi *MetricsIndex) GetTagNames(prefix string) []string {
+	return nil
+}
+
+func (mi *MetricsIndex) GetAllTagNames() []string {
+	return nil
+}
+
+func (mi *MetricsIndex) GetTagValues(tagNameStr, prefix string) []string {
+	return nil
+}
+
+func (mi *MetricsIndex) GetAllTagValues(tagNameStr string) []string {
+	return nil
+}
+
+func (mi *MetricsIndex) GetMetricNameByID(id types.MetricID) (string, error) {
+	return "", nil
+}
+
+func (mi *MetricsIndex) GetMetricsNamesByIDs(ids []types.MetricID) ([]string, error) {
+	return nil, nil
+}
+
+/*
 func (mi *MetricsIndex) AddMetric(metric types.Metric) {
 	metricID := metric.CalcMetricID()
 
@@ -113,3 +156,5 @@ func (mi *MetricsIndex) GetEnumeratorByCondition(tn types.TagName, tv types.TagV
 	}
 	return enumerator, nil
 }
+
+*/
